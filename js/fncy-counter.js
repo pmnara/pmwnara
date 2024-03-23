599da452c2f30715629db2e3c75b5f71356132ec
pmwnara <pramodnaarayana@gmail.com>
2024-03-24 03:56:03 +0530

    added a add fuction to fncycounter

--- js/fncy-counter.js
+++ js/fncy-counter.js
@@ -1,1 +1,11 @@
-console.log('js is working')
+console.log('js is working');
+
+const increaseButton = document.querySelector(".counter__button--increase");
+const counterValue = document.querySelector('.counter__value');
+
+increaseButton.addEventListener('click',function(){
+    const currentValue = counterValue.textContent;
+    const currentValueToNumber = +currentValue;
+    const newValue = currentValueToNumber+1;
+    counterValue.textContent = newValue;
+});
