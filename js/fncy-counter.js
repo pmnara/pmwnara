ecdcc2d8408fb0597ac18be5d1b41ea34e20dbf5
pmwnara <pramodnaarayana@gmail.com>
2024-03-24 04:06:19 +0530

    more fucntions
    added a reset and a minus function to the value, yet struggling to get the value to stop counting at zero

--- js/fncy-counter.js
+++ js/fncy-counter.js
@@ -2,6 +2,8 @@ console.log('js is working');
 
 const increaseButton = document.querySelector(".counter__button--increase");
 const counterValue = document.querySelector('.counter__value');
+const resetButton = document.querySelector('.counter__reset-button');
+const decreaseButton = document.querySelector('.counter__button--decrease');
 
 increaseButton.addEventListener('click',function(){
     const currentValue = counterValue.textContent;
@@ -9,3 +11,18 @@ increaseButton.addEventListener('click',function(){
     const newValue = currentValueToNumber+1;
     counterValue.textContent = newValue;
 });
+
+resetButton.addEventListener('click', function(){
+    counterValue.textContent=0;
+});
+
+decreaseButton.addEventListener('click', function(){
+    const currentValue = counterValue.textContent;
+    const currentValueToNumber = +currentValue;
+    const minusValue = currentValue-1;
+    counterValue.textContent= minusValue;
+    if(minusValue==0){
+        
+    }
+});
+
